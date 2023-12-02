ed02bfcd7be31d156765b12493452421baaeaeab
Gabriel Silva <gabriel.silva781.09@gmail.com>
2023-12-02 15:35:35 -0300

    feat: added users controllers * jest

--- src/server/controllers/users/index.ts
+++ src/server/controllers/users/index.ts
@@ -2,7 +2,7 @@ import * as create  from './Create';
 import * as getAll  from './GetAll';
 import * as getbyid  from './GetById';
 import * as updatebyid  from './UpdateById';
-import * as deletebyid from './deleteById';
+import * as deletebyid from './DeleteById';
 
 export const UsersController = {
     ...create,
