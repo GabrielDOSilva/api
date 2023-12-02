20632d97582b47a1beec8750c5b5af5b9734a3ce
Gabriel Silva <gabriel.silva781.09@gmail.com>
2023-12-02 15:39:56 -0300

    feat: added news raviables for users

--- src/server/controllers/users/Create.ts
+++ src/server/controllers/users/Create.ts
@@ -8,13 +8,17 @@ import { StatusCodes } from 'http-status-codes';
 
 interface IUsers {
     name: string;
+    sobrenome: string;
     email: string;
+    cpf: number;
 }
 
 export const createValidation = validation((getSchema) => ({
     body: getSchema<IUsers>(yup.object().shape({
         name: yup.string().required(),
-        email: yup.string().required().email()
+        sobrenome: yup.string().required(),
+        email: yup.string().required().email(),
+        cpf: yup.number().required().min(11)
     })),
 
 }));
